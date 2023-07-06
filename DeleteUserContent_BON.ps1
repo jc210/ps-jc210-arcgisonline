974d376c37b51386d69596e78c4167cdfac11523
John Castillo <john@flyingtitan.com>
2023-07-06 18:06:14 -0500

    Much needed updates
    - Uses TLS 1.2 for web connections.
    - Attempts to remove delete protection for files and folders.
    - ArcGIS Identity, also known as named user, grants a short-lived access token. Script will temporarily disable the user accounts so any access tokens will expire.

--- DeleteUserContent_BON.ps1
+++ DeleteUserContent_BON.ps1
@@ -1,56 +1,51 @@
 ﻿<# ------------------------------------------------------------------
 Script: DeleteUserContent_BON.ps1
 Author: John Castillo
-Purpose:
-Uses the REST interface to remove data from ArcGIS Online and a Portal for ArcGIS server if required.
+Purpose: Use the ArcGIS REST API to delete user items within your ArcGIS Online organization.
 All actions are logged to a local .html file and also emailed. These settings are in the SendEmail_Exit function.
-
-
 ------------------------------------------------------------------ #>
 
 # ------------------------------------------
-param (
-    [string]$BonGroup = "learner" # Name of user group.
-    )
-$GroupTotalMembers = 16 # This is the number of members per user group. Usually, <user>00 - <user>15
-$Global:StarDate = get-Date #Saves when script was started. Learn more: http://technet.microsoft.com/en-us/library/ee692801.aspx
-$Global:body = @() # To capture our output to variable.
-$Global:headers = @{"Accept" = "application/json";"Content-Type" = "application/json"}
+$TSAccountGroups = @("alpha","beta") # Create list of group accounts
+$GroupTotalMembers = 21 # This is the number of members per user group. Naming example: alpha00 - alpha20
+$Global:StarDate = get-Date # Saves when script was started. Learn more: http://technet.microsoft.com/en-us/library/ee692801.aspx
+$Global:myLogFile = ".\log\" + $BonGroup + "-" + $StarDate.ToString("yyyyMMdd-HHmmss") + ".html" # See Function SendEmail_Exit
+$Global:body = @() # Capture our output to this variable
+$Global:headers = @{"Content-Type" = "application/x-www-form-urlencoded"}
+$body += '<!DOCTYPE html>'
 $body += '<BODY style="font-family: Calibri; font-size: 11pt;">'
+$body += '<style>#HZ_List ul {margin: 0;padding: 0;} #HZ_List li {list-style-type: none;display: inline;padding: 0 10px;border-left: solid 1px black;} #HZ_List li:first-child {border-left: none;}</style>'
 $body += '<b>Script generated email.</b> ' + ($StarDate.DateTime) + '<p>'
 # ------------------------------------------
 
-
+#Esri will require TLS 1.2 connections for ArcGIS Online services starting on April 16, 2019 
+#This will force the use of TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 
 # ArcGIS Online Settings -------------------
 $Global:ArcOnlineDomain = 'yourcoolsite.maps.arcgis.com'
-$Global:ArcOnlineKey = "_" + "yourcoolsite" # The user name NameID_<url_key_for_org> will be created by ArcGIS Online in its user store.
+$Global:ArcOnlineKey = "_" + "yourcoolsite" # The user name NameID_<url_key_for_org> will be created by ArcGIS Online in its user store
 $Global:arcAdmin = "yourOrgAdminUsername"
 $arcCryptoPass = Get-Content -Path .\AGOL-AdminUsername.txt | ConvertTo-SecureString
 $arcCredentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $arcAdmin,$arcCryptoPass
-$Global:arcPass = $arcCredentials.GetNetworkCredential().password
+$Global:arcPass = $arcCredentials.GetNetworkCredential().password # Comment this out to use plain text password example below
 #$Global:arcPass = "Password"
 # ------------------------------------------
-
-# Portal for ArcGIS Settings ---------------
-$Global:portalDomain = 'bon.esri.com'
-$Global:portalAdmin = "yourPortalAdminUsername"
-$portalCryptoPass = Get-Content -Path .\BON-AdminUsername.txt | ConvertTo-SecureString
-$portalCredentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $portalAdmin,$portalCryptoPass
-$portalPass = $portalCredentials.GetNetworkCredential().password
-#$Global:portalPass = "Password"
-# ------------------------------------------
-
-
     
 
-$body += '<b>ArcGIS Online Org: </b><a href="http://' + $ArcOnlineDomain + '" alt=ArcGIS.Server.URL>' + $ArcOnlineDomain + '</a><b> BON Portal: </b><a href="http://' + $portalDomain + '" alt=ArcGIS.Server.URL>' + $portalDomain + '</a><p>'
+$body += '<b>ArcGIS Online Org: </b><a href="http://' + $ArcOnlineDomain + '" alt=ArcGIS.Server.URL>' + $ArcOnlineDomain + '</a><p>'
 $body += "<hr>"
 
 Function Generate_Token ($server, $admin, $adminpassword) {
     $body += $body
-    $genToken = Invoke-RestMethod -Headers $headers -Uri "https://${server}/sharing/generateToken?f=json&request=gettoken&username=${admin}&password=${adminpassword}&referer=http://www.arcgis.com"
+    Write-Host "Using ${server}"
+    # Construct our variables used for the token request
+    $t_URL = "https://${server}/sharing/rest/generateToken?"
+    $t_PostBody = @{"username" = "$admin";"password" = "$adminpassword";"client" = "requestip";"f" = "json"}
+    # Send token request and store response as a variable
+    $genToken = Invoke-RestMethod -Uri $t_URL -Headers $headers -Method Post -Body $t_PostBody
+
     $genToken.token
 
 #    Write-Host "token: "  $token 
@@ -58,21 +53,41 @@ Function Generate_Token ($server, $admin, $adminpassword) {
 IF([string]::IsNullOrWhiteSpace($genToken.token)) {            
     Write-Host "Given string is NULL or having WHITESPACE" 
     $body += "<font color=red>Token generation failed for <b>${server}</b> JSON: " + ($genToken | Out-String) +"</font>"
-    SendEmail_Exit $body            
+    SendEmail_Exit $body
+    Exit            
 } else {            
-   # Write-Host "Given string has a value"
+    Write-Host "Given string has a value"
 }
-
-
     #$genToken = "${AGOLrootURL}/generateToken?f=json&request=gettoken&username=${arcAdmin}&password=${arcPass}&referer=http://www.arcgis.com"
     #$token = Invoke-WebRequest -Uri $genToken  | ConvertFrom-Json | Select -ExpandProperty token
     #Write-Host "HELLO! " + $genToken.psobject.Properties.name # This shows what objects are in the array CONTENT.
 }
 
+Function Enable_User ($server, $token, $user) {
+    $vRootURL = "https://${server}/sharing/rest"
+    $vCommunityURL = "${vRootURL}/community/users"    
+    
+    $JsonCommon = "?token=${token}&f=json"
+    $mylog = @()
+    
+    $EnableUser = Invoke-RestMethod -Headers $headers -Method Post -Uri ($vCommunityURL + "/" + $user + "/enable" + $JsonCommon)
+           
+    If ($EnableUser.success -eq "true") {
+        $mylog += "<font color=#cf33ff style=background-color:GhostWhite;><b>Enable $user account:</b> $EnableUser"  #background GhostWhite
+        Write-Host "Is $user account enabled? "$EnableUser -ForegroundColor DarkGreen -BackgroundColor White
+        $mylog += " </font><br>" 
+    }
+    else {
+        $mylog += "<font color=white style=background-color:Tomato;><b>Enable $user account error detected:</b> $EnableUser"  #background tomato
+        Write-Host "Disable $user account error detected: "$EnableUser -ForegroundColor Red -BackgroundColor White
+        $mylog += " </font><br>" 
+    }
+$mylog | Out-String
+}
+
 Function Get_User_Content ($server, $token, $user) {
     $vRootURL = "https://${server}/sharing/rest"
     $vUserURL = "${vRootURL}/content/users"
-    $vGroupURL = "${vRootURL}/groups" # http://<community-url>/groups/<groupId>
     $vCommunityURL = "${vRootURL}/community/users"    
     
     $JsonCommon = "?token=${token}&f=json"
@@ -132,30 +147,46 @@ $mylog | Out-String
 Function Delete_User_Content ($server, $token, $user) {
     $vRootURL = "https://${server}/sharing/rest"
     $vUserURL = "${vRootURL}/content/users"
-    $vGroupURL = "${vRootURL}/groups" # http://<community-url>/groups/<groupId>
-    $vCommunityURL = "${vRootURL}/community/users"    
     
     $JsonCommon = "?token=${token}&f=json"    
     $mylog = @()
 
-
     IF($GetUserContent.items.Count -eq 0) {            
         Write-Host "Files is NULL or having WHITESPACE " $server 
-        $mylog += $exterminateFiles + "No files found on " + $server + "<br>"           
+        #$mylog += $exterminateFiles + "No files found on " + $server + "<br>"           
     } else {            
-         Write-Host $exterminateFiles
+        Write-Host $exterminateFiles
+        $removeprotection = $GetUserContent.items.ForEach({Invoke-RestMethod -Headers $headers -Method Post -Uri ($vUserURL + "/" + $user+ "/items/" + $_.id + "/unprotect" + $JsonCommon)})
+        $mylog += "Removing delete protections" + $removeprotection + "<br>" 
         $exterminateFiles = $GetUserContent.items.ForEach({Invoke-RestMethod -Headers $headers -Method Post -Uri ($vUserURL + "/" + $user+ "/items/" + $_.id + "/delete" + $JsonCommon)})
-        $mylog += $exterminateFiles + "<br>"        
+        IF ($exterminateFiles -match "error") {
+            $mylog += "<font color=red>"
+            $mylog += $exterminateFiles + "</font><br>"
+            Write-Host "PAINT IT RED"
+            }
+            else {
+            $mylog += $exterminateFiles + "</font><br>"
+            Write-Host "PAINT IT BLACK"
+            }        
     }
     
     
     IF($GetUserContent.folders.Count -eq 0) {            
         Write-Host "folders is NULL or having WHITESPACE " $server 
-        $mylog += $exterminateFolders + "No folders found " + $server + "<br>"           
+        #$mylog += $exterminateFolders + "No folders found " + $server + "<br>"           
     } else {            
         Write-Host $exterminateFolders
         $exterminateFolders = $GetUserContent.folders.ForEach({Invoke-RestMethod -Headers $headers -Method Post -Uri ($vUserURL + "/" + $user+ "/" + $_.id + "/delete" + $JsonCommon)})
-        $mylog += $exterminateFolders + "<br>"       
+        IF ($exterminateFolders -match "error") {
+            $mylog += "<font color=red>"
+            $mylog += $exterminateFolders + "</font><br>"
+            Write-Host "PAINT IT RED"
+            }
+            else {
+            $mylog += $exterminateFolders + "</font><br>"
+            Write-Host "PAINT IT BLACK"
+            }  
+               
     }    
 
 $mylog | Out-String
@@ -165,83 +196,66 @@ $mylog | Out-String
 
 Function SendEmail_Exit ($message) {
 $body += $message | Out-String
-$body += '<p><center>END OF LINE</center><p></BODY>'
+$body += '<p><center>######  END OF LINE  #####</center><p></BODY>'
 
 $body = $body | Out-String # Body of email needs to be string.
-$myLogFile = ".\log\" + $BonGroup + "-" + $StarDate.ToString("yyyyMMdd-HHmmss") + ".html"
 $body | Out-File $myLogFile # Saves as HTML file.
 
 $amiUser = "yourAmazonSESuser"
 $amiPass = Get-Content -Path .\AmazonSES-SMTP.txt | ConvertTo-SecureString
 $amiCredentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $amiUser,$amiPass
-#$companyUser = "domain\yourUsername" #username for sending emails
-#$companyCredentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $esriUser,(Get-Content -Path .\jcastillo-SMTP.txt | ConvertTo-SecureString)
 
 $email = @{
-    From = "BON " + $BonGroup + " <you@yourcompany.com>"
-    To = "you@yourcompany.com"
-    CC = "you@yourcompany.com" 
+    From = "BON CleanUp" + "<you@yourcompany.com>"
+    To = "user@yourcompany.com"
+    CC = "otheruser@yourcompany.com" 
     Subject = " BON User " + $BonGroup + " cleanup Log"
-    #Credential = $companyCredentials
     Credential = $amiCredentials
     SMTPServer = "email-smtp.us-east-1.amazonaws.com"
     Body = $body
 
 
-    #From = "BON " + $BonGroup + " <you@yourcompany.com>"
-    #To = "you@yourcompany.com"
-    #CC = "you@yourcompany.com" 
-    #Subject = " BON User " + $BonGroup + " cleanup Log"
-    #Credential = $companyCredentials
-    #Credential = "companyUser"
-    #SMTPServer = "smtp.yourcompany.com"
-    #Body = $body
 }
 
-Send-MailMessage @email -BodyAsHtml -UseSsl
+Write-Host "Trying to send email..."
+Send-MailMessage @email -BodyAsHtml -UseSsl -Port 587
 Exit
 }
 
-# ArcGIS Online ----------------------------
+Function TSAccountMembers_routine ($BonGroup) {
+    $mylog = @()
+    For ( $i = 0 ; $i -lt $GroupTotalMembers ; $i++ ) {  # The -lt (Less than) comparison operator is used since PS only counts whole numbers.
+        $iBonUser = $BonGroup + $i.ToString("00")
+    # ------------------------------------------
+    #
+        Write-Host "User: " $iBonUser
+        
+    
+    # ArcGIS Online
+        $mylog += "<font style=background-color:Snow;><b>User: </b>" + $iBonUser + $ArcOnlineKey + "</font></style><br>"
+        $mylog += Disable_User $ArcOnlineDomain $ArcOnlineToken ($iBonUser + $ArcOnlineKey)
+        $mylog += Get_User_Content $ArcOnlineDomain $ArcOnlineToken ($iBonUser + $ArcOnlineKey)
+        $mylog += Delete_User_Content $ArcOnlineDomain $ArcOnlineToken ($iBonUser + $ArcOnlineKey)
+        $mylog += Enable_User $ArcOnlineDomain $ArcOnlineToken ($iBonUser + $ArcOnlineKey)
+    
+    }
+    
+    $mylog | Out-String
+    }
+    
+
+
 $Global:ArcOnlineToken = Generate_Token $ArcOnlineDomain $arcAdmin $arcPass
 Write-Host "AGOL "$Global:ArcOnlineToken
 $body += "<font color=green>Token generated for ${ArcOnlineDomain}</font><br>" 
-# ------------------------------------------
 
-# Portal for ArcGIS ------------------------
-#$Global:portalDomainToken = Generate_Token ($portalDomain + "/arcgis") $portalAdmin $portalPass
-#Write-Host "Portal "$Global:portalDomainToken
-#$body += "<font color=green>Token generated for ${portalDomain}</font><br>" 
-# ------------------------------------------
+foreach ($group in $TSAccountGroups) {
 
-For ( $i = 0 ; $i -lt $GroupTotalMembers ; $i++ ) {  # The -lt (Less than) comparison operator is used since PS only counts whole numbers.
-    $iBonUser = $BonGroup + $i.ToString("00")
-
-    $iPortals = ($portalDomain + "/arcgis") + " " + $portalDomainToken + " " + $iBonUser
-# ------------------------------------------
-
-    Write-Host "Username: " $iBonUser
-    
-
-# ArcGIS Online ----------------------------
-    $body += "<b>ArcGIS Online: </b>" + $iBonUser + $ArcOnlineKey + "</style><br>"
-    $body += Get_User_Content $ArcOnlineDomain $ArcOnlineToken ($iBonUser + $ArcOnlineKey)
-    $body += Delete_User_Content $ArcOnlineDomain $ArcOnlineToken ($iBonUser + $ArcOnlineKey)
-# ------------------------------------------
-
-# Portal for ArcGIS ------------------------
-#    $body += "<b>BON Portal: </b>" + $iBonUser + "</style><br>"   
-#    $body += Get_user_Content ($portalDomain + "/arcgis") $portalDomainToken $iBonUser
-#    $body += Delete_User_Content ($portalDomain + "/arcgis") $portalDomainToken $iBonUser
-#    $body += "<hr>"
-# ------------------------------------------
-
-
-}
+    $body += TSAccountMembers_routine $group
+ 
+  }
 
 SendEmail_Exit $body
-
-
 
 
 Exit # If script runs properly, we actually exit in the SendEmail_Exit function. 
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 PowerShell script that uses the ArcGIS Online REST interface for bulk administrative tasks. 
 
 File: DeleteUserContent_BON.ps1
-About: Searches a group of users for content and deletes files and folders. Writes an HTML log file and will send email.
+About: Searches sequential numbered users of an ArcGIS Org and deletes all items for each user. Writes to an HTML log file and will send as an email.
